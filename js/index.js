beb627fc0b1fbe5dacf6cb3c916aa953fce09125
koral14 <105457134+koral14@users.noreply.github.com>
2023-11-20 13:47:34 -0800

    pudated js file

--- js/index.js
+++ js/index.js
@@ -4,9 +4,9 @@ var dateAndTime = today.getMonth() + "/" + today.getDate() + "/" + today.getFull
 const thisYear = today.getFullYear();
 const thisHour = today.getHours();
 // select the <footer> element from the DOM and store it in a variable
-// const footer = document.querySelector('footer');
-// const copyright = document.createElement('p');
-// copyright.innerHTML = `&copy; Olga Musteata ${thisYear}`;
+const footer = document.querySelector('footer');
+const copyright = document.createElement('p');
+copyright.innerHTML = `&copy; Olga Musteata ${thisYear}`;
 // append the copyright element to the footer
 footer.appendChild(copyright);
 // Add Skills Section
@@ -19,78 +19,79 @@ const skillsList = skillsSection.querySelector('ul');
 for (let i = 0; i < skills.length; i++) {
     let skill = document.createElement('li');
     skill.innerHTML = skills[i];
-    skillsList.appendChild(skill);
+    // skillsList.appendChild(skill);
 }
 
 // target the form and use an event listener to listen for form submit
 const messageForm = document.getElementsByName('leave_message');
 
-messageForm[0].addEventListener('submit', (event) => {
-    event.preventDefault();
-    const name = event.target.name.value;
-    const email = event.target.email.value;
-    const message = event.target.message.value;
-    console.log(name + " " + email + " " + message);
-    const messageSection = document.getElementById('messages');
-    const messageList = messageSection.querySelector('ul');
-    const newMessage = document.createElement('li');
-    // create the message
-    newMessage.innerHTML = `<span>${message}</span><br>
-        ${dateAndTime} from <a href="mailto:${email}">${name}</a><br>`;
+// messageForm[0].addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     const name = event.target.name.value;
+//     const email = event.target.email.value;
+//     const message = event.target.message.value;
+//     console.log(name + " " + email + " " + message);
+//     const messageSection = document.getElementById('messages');
+//     const messageList = messageSection.querySelector('ul');
+//     const newMessage = document.createElement('li');
+//     // create the message
+//     newMessage.innerHTML = `<span>${message}</span><br>
+//         ${dateAndTime} from <a href="mailto:${email}">${name}</a><br>`;
 
-    //edit button
-    const editButton = document.createElement('button');
-    editButton.innerText = 'edit';
-    editButton.type = 'button';
-    editButton.id = 'editButton1';
-    editButton.addEventListener('click', (e) => {
-        const button = e.target;
-        const li = button.parentNode;
-        const ul = li.parentNode;
-        if (e.target.tagName === 'BUTTON') {
-            if (button.textContent === 'remove') {
-                ul.removeChild(newMessage);
-            } else if (button.textContent === 'edit') {
-                const span = newMessage.childNodes[0];
-                const input = document.createElement('input');
-                input.type = 'text';
-                input.value = span.innerText;
-                newMessage.insertBefore(input, span);
-                newMessage.removeChild(span);
-                button.textContent = 'save';
-            } else if (button.textContent === 'save') {
-                const input = newMessage.childNodes[0];
-                const span = document.createElement('span');
-                span.textContent = input.value;
-                newMessage.insertBefore(span, input);
-                newMessage.removeChild(input);
-                button.textContent = 'edit';
-            }
-        }
-    }); 
-    newMessage.appendChild(editButton);
-    const title_messages = document.getElementById('title_h2');
+//     //edit button
+//     const editButton = document.createElement('button');
+//     editButton.innerText = 'edit';
+//     editButton.type = 'button';
+//     editButton.id = 'editButton1';
+//     editButton.addEventListener('click', (e) => {
+//         const button = e.target;
+//         const li = button.parentNode;
+//         const ul = li.parentNode;
+//         if (e.target.tagName === 'BUTTON') {
+//             if (button.textContent === 'remove') {
+//                 ul.removeChild(newMessage);
+//             } else if (button.textContent === 'edit') {
+//                 const span = newMessage.childNodes[0];
+//                 const input = document.createElement('input');
+//                 input.type = 'text';
+//                 input.value = span.innerText;
+//                 newMessage.insertBefore(input, span);
+//                 newMessage.removeChild(span);
+//                 button.textContent = 'save';
+//             } else if (button.textContent === 'save') {
+//                 const input = newMessage.childNodes[0];
+//                 const span = document.createElement('span');
+//                 span.textContent = input.value;
+//                 newMessage.insertBefore(span, input);
+//                 newMessage.removeChild(input);
+//                 button.textContent = 'edit';
+//             }
+//         }
+//     }); 
+//     newMessage.appendChild(editButton);
+//     const title_messages = document.getElementById('title_h2');
     // remove button
     const removeButton = document.createElement('button');
     removeButton.innerText = 'remove';
     removeButton.type = "button";
     removeButton.id = 'removeButton2';
-    removeButton.addEventListener('click', () => {
-        const entry = removeButton.parentNode;
-        entry.remove();
-        // remove 'Messages' title if no messages
-        if (messageList.children.length < 1) {
-            title_messages.innerText = '';
-        }
-    });
-    newMessage.appendChild(removeButton);
-    messageList.appendChild(newMessage);
+    // removeButton.addEventListener('click', () => {
+    //     const entry = removeButton.parentNode;
+    //     entry.remove();
+    //     // remove 'Messages' title if no messages
+    //     if (messageList.children.length < 1) {
+    //         title_messages.innerText = '';
+    //     }
+    // });
+    // newMessage.appendChild(removeButton);
+    // messageList.appendChild(newMessage);
     // 'Messages' title appears only when there is a message:
-    if (newMessage.innerText.length > 1) {
-        title_messages.innerText = 'Messages:';
-    } 
-    messageForm.item(0).reset();
-});
+    // if (newMessage.innerText.length > 1) {
+    //     title_messages.innerText = 'Messages:';
+    // } 
+    // messageForm.item(0).reset();
+// }
+// );
 
 fetch('https://api.github.com/users/koral14/repos')
     .then(response => response.json())
@@ -98,17 +99,17 @@ fetch('https://api.github.com/users/koral14/repos')
     .catch(error => console.log('Looks like there was a problem!!!', error))
 
 function generateRepos(data) {
-    var projectSection = document.getElementById('projects');
-    var projectList = projectSection.querySelector('ul');
-    for (let i=0; i < data.length; i++) {
-        var project = document.createElement('li');
-        project.innerHTML = `
-            <a href="${data[i].html_url}"><b>${data[i].name}</b></a> 
-            was created at ${getDate1(data[i].created_at)}. 
-            <br> <b>Short description: </b>${data[i].description}
-        `;
-        projectList.appendChild(project);
-    }
+    // var projectSection = document.getElementById('projects');
+    // var projectList = projectSection.querySelector('ul');
+    // for (let i=0; i < data.length; i++) {
+    //     var project = document.createElement('li');
+    //     project.innerHTML = `
+    //         <a href="${data[i].html_url}"><b>${data[i].name}</b></a> 
+    //         was created at ${getDate1(data[i].created_at)}. 
+    //         <br> <b>Short description: </b>${data[i].description}
+    //     `;
+    //     projectList.appendChild(project);
+    // }
 }
 
 // extracts date from template: 2022-08-20T02:28:51Z
